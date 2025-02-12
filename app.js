49d75bd95fcebe9b037be1f571442cb8116909a1
Pedro Pazini <pedro.pzn23@outlook.com>
2025-02-11 22:49:34 -0300

    feat: adding the restart option and finishing the project

--- app.js
+++ app.js
@@ -35,7 +35,6 @@ function sortear() {
         numero = obterNumeroAleatorio(de, ate);
         sorteados.push(numero);
     }
-    alert (sorteados)
 
     mostrarNumero('resultado', `Números sorteados: ${sorteados}`);
 
@@ -44,6 +43,14 @@ function sortear() {
 
 function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random()* (max - min + 1)) + min;
+}
+
+function reiniciar() {
+    mostrarNumero('resultado', `Números sorteados: nenhum até agora`);
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    alterarStatusBotao();
 }
 
 

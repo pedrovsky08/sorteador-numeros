0140b3fba93ce3fddd1577aa595e2862419a4324
Pedro Pazini <pedro.pzn23@outlook.com>
2025-02-11 22:35:34 -0300

    feat: implementing secundary function to previous commit

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ function alterarStatusBotao() {
     if (botao.classList.contains('container__botao-desabilitado')) {
         botao.classList.remove('container__botao-desabilitado');
         botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');       
     }
 }
 
